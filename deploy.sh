5dd0ce5daf07c3ad99cc11a94385997cf8c9f3d6
Roman Birg <romanbirg@gmail.com>
2012-02-12 23:15:05 -0600

    add more devices to build :D

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 BUILD_ROOT=~/aokp
 
 # setup folders
-rm -R ./upload
-mkdir ./upload
+rm -R ../upload
+mkdir ../upload
 
 # clean
 cd $BUILD_ROOT
@@ -19,17 +19,22 @@ fi
 # build_device <lunch combo> <device name>
 #
 cd ..
-#./build_device.sh 7 maguro
-#./build_device.sh 8 toro
+./build_device.sh 7 maguro
+./build_device.sh 8 toro
 ./build_device.sh 5 crespo
 ./build_device.sh 6 crespo4g
 ./build_device.sh 9 p4wifi
+./build_device.sh 10 tenderloin
+./build_device.sh 11 vivow
 ./build_device.sh 12 p4
 ./build_device.sh 13 p4vzw
 ./build_device.sh 10 tenderloin
 ./build_device.sh 11 vivow
 ./build_device.sh 14 stingray
 ./build_device.sh 15 wingray
+./build_device.sh 16 supersonic
+./build_device.sh 17 inc
+./build_device.sh 18 vzwtab
 cd upload
 
 # done!
