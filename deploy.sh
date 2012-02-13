0a64bbb99316f84a52046dc0d357f3e4bb61ffe7
Roman Birg <romanbirg@gmail.com>
2012-02-12 23:30:33 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,24 +19,25 @@ fi
 #
 # build_device <lunch combo> <device name>
 #
-cd ..
-$BUILDBOT/build_device.sh 7 maguro
-$BUILDBOT/build_device.sh 8 toro
-$BUILDBOT/build_device.sh 5 crespo
-$BUILDBOT/build_device.sh 6 crespo4g
-$BUILDBOT/build_device.sh 9 p4wifi
-$BUILDBOT/build_device.sh 10 tenderloin
-$BUILDBOT/build_device.sh 11 vivow
-$BUILDBOT/build_device.sh 12 p4
-$BUILDBOT/build_device.sh 13 p4vzw
-$BUILDBOT/build_device.sh 10 tenderloin
-$BUILDBOT/build_device.sh 11 vivow
-$BUILDBOT/build_device.sh 14 stingray
-$BUILDBOT/build_device.sh 15 wingray
-$BUILDBOT/build_device.sh 16 supersonic
-$BUILDBOT/build_device.sh 17 inc
-$BUILDBOT/build_device.sh 18 vzwtab
-cd upload
+cd $BUILDBOT
+./build_device.sh 7 maguro
+./build_device.sh 8 toro
+./build_device.sh 5 crespo
+./build_device.sh 6 crespo4g
+./build_device.sh 9 p4wifi
+./build_device.sh 10 tenderloin
+./build_device.sh 11 vivow
+./build_device.sh 12 p4
+./build_device.sh 13 p4vzw
+./build_device.sh 10 tenderloin
+./build_device.sh 11 vivow
+./build_device.sh 14 stingray
+./build_device.sh 15 wingray
+./build_device.sh 16 supersonic
+./build_device.sh 17 inc
+./build_device.sh 18 vzwtab
+
+cd ~/upload
 
 # done!
 md5sum aokp_*
