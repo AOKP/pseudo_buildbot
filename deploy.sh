93f49a560f11f62f5c24d7facf1e5a95fa80b985
KhasMek <Boushh@gmail.com>
2012-02-15 18:22:09 -0500

    Initial commit of complete rebasing of bot.     Now uses envsetup from the android build system for certain     calls and information about vendor/target/etc. This allows it     to be much more scalable and portable to other vendors.
        Code is insanely messy right now, but I need to get some sort
        of commit history going on this, for my memories sake.

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,30 @@
 #!/bin/bash
 
-BUILD_ROOT=~/aokp
-BUILDBOT=~/buildbot
-
 # setup folders
-rm -R ../upload
-mkdir ../upload
+#OUTD=$(echo $(cd ../test && pwd))
+#rm -R $OUTD
+#mkdir $OUTD
 
 # clean
+BUILD_ROOT=`pwd`
+echo "$BUILD_ROOT"
 cd $BUILD_ROOT
-repo sync
+#repo sync
 . build/envsetup.sh
 
-if [ $1 = "clean" ]; then 
+if [ "$1" = "clean" ]; then
 	make clean
 fi
 
 #
 # build_device <lunch combo> <device name>
 #
-cd $BUILDBOT
-./build_device.sh 7 maguro
-./build_device.sh 8 toro
-./build_device.sh 5 crespo
-./build_device.sh 6 crespo4g
-./build_device.sh 9 p4wifi
-./build_device.sh 10 tenderloin
-./build_device.sh 11 vivow
-./build_device.sh 12 p4
-./build_device.sh 13 p4vzw
-./build_device.sh 10 tenderloin
-./build_device.sh 11 vivow
-./build_device.sh 14 stingray
-./build_device.sh 15 wingray
-./build_device.sh 16 supersonic
-./build_device.sh 17 inc
-./build_device.sh 18 vzwtab
+#cd $BUILDBOT
+#./build_device.sh 8 toro
+./vendor/aokp/bot/build_device.sh 8 toro
+./vendor/aokp/bot/build_device.sh 9 p4wifi
 
-cd ~/upload
+#cd $OUTD
 
 # done!
-md5sum aokp_*
+#md5sum aokp_*
