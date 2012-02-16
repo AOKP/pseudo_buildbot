66008871bda35bf7854083abc82b83b611a0f5b7
KhasMek <Boushh@gmail.com>
2012-02-15 19:21:40 -0500

    major cleanup

--- build_device.sh
+++ build_device.sh
@@ -10,20 +10,16 @@ TARGET_VENDOR=$(echo $TARGET_PRODUCT | cut -f1 -d '_')
 VER=$(cat vendor/$TARGET_VENDOR/products/common_versions.mk | grep "TARGET_PRODUCT" | cut -f3 -d '_' | cut -f1 -d ' ')
 ZIP=$(find $(echo $ANDROID_PRODUCT_OUT) -maxdepth 1 -name $TARGET_VENDOR_*-squished.zip)
 OUTD=$(echo $(cd ../test && pwd))
-#ZIP=/home/khasmek/Android/AOKP/out/target/product/toro/aokp_toro_build-23-squished.zip
 
 # $1 should be lunch combo
 # $2 should be device name
 
 # build
 make -j9 bacon
-#ZIP=$(find $(echo $ANDROID_PRODUCT_OUT) -maxdepth 1 -name $TARGET_VENDOR_$2*-squished.zip)
-#OUTD=$(echo $(cd ../test && pwd))
+
 # clean out of previous zip
 OUTZ=$(echo $TARGET_PRODUCT)_$VER.zip
 rm -rf $OUTD/$OUTZ
-#mkdir $OUTD
-#NAME=aokp_$2_$BUILD.zip
 cp $ZIP $OUTD/$OUTZ
 
 # finish
@@ -31,8 +27,8 @@ echo "$2 build complete"
 
 # md5sum list
 cd $OUTD
-#md5sum aokp_*
 md5sum $OUTZ | cat >> md5sum
+
 # upload
 echo "checking on upload reference file"
 
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# setup folders
-#OUTD=$(echo $(cd ../test && pwd))
-#rm -R $OUTD
-#mkdir $OUTD
-
 # clean
 BUILD_ROOT=`pwd`
-echo "$BUILD_ROOT"
 cd $BUILD_ROOT
 #repo sync
 . build/envsetup.sh
@@ -19,12 +13,5 @@ fi
 #
 # build_device <lunch combo> <device name>
 #
-#cd $BUILDBOT
-#./build_device.sh 8 toro
 ./vendor/aokp/bot/build_device.sh 8 toro
 ./vendor/aokp/bot/build_device.sh 9 p4wifi
-
-#cd $OUTD
-
-# done!
-#md5sum aokp_*
