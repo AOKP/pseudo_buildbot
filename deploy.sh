c5484e0a954188c00e1dd4f79adf73482a95a231
KhasMek <Boushh@gmail.com>
2012-02-15 19:35:59 -0500

    add in missing devices

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,20 @@ fi
 #
 # build_device <lunch combo> <device name>
 #
+./vendor/aokp/bot/build_device.sh 7 maguro
 ./vendor/aokp/bot/build_device.sh 8 toro
+./vendor/aokp/bot/build_device.sh 5 crespo
+./vendor/aokp/bot/build_device.sh 6 crespo4g
 ./vendor/aokp/bot/build_device.sh 9 p4wifi
+./vendor/aokp/bot/build_device.sh 10 tenderloin
+./vendor/aokp/bot/build_device.sh 11 vivow
+./vendor/aokp/bot/build_device.sh 12 p4
+./vendor/aokp/bot/build_device.sh 13 p4vzw
+./vendor/aokp/bot/build_device.sh 10 tenderloin
+./vendor/aokp/bot/build_device.sh 11 vivow
+./vendor/aokp/bot/build_device.sh 14 stingray
+./vendor/aokp/bot/build_device.sh 15 wingray
+./vendor/aokp/bot/build_device.sh 16 supersonic
+./vendor/aokp/bot/build_device.sh 17 inc
+./vendor/aokp/bot/build_device.sh 18 vzwtab
+
