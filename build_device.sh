3f45f572a2bb81c554e11877670415497ee7d095
KhasMek <Boushh@gmail.com>
2012-02-25 14:53:57 -0500

    bacon check

--- build_device.sh
+++ build_device.sh
@@ -10,6 +10,14 @@ lunch $1
 
 TARGET_VENDOR=$(echo $TARGET_PRODUCT | cut -f1 -d '_')
 
+# bacon check
+if [ "$(grep -m 1 bacon build/envsetup.sh)" = "" ]; then
+    echo "Y U NO MAKE BACON?!"
+    BACON=false
+else
+    BACON=true
+fi
+
 # create log dir if not already present
 if test ! -d "$ANDROID_PRODUCT_OUT"
     echo "$ANDROID_PRODUCT_OUT doesn't exist, creating now"
@@ -17,10 +25,18 @@ if test ! -d "$ANDROID_PRODUCT_OUT"
 fi
 
 # build
-make -j$(grep processor /proc/cpuinfo | wc -l) bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+if [ "$BACON" = "true" ]; then
+    make -j$(grep processor /proc/cpuinfo | wc -l) bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+else
+    make -j$(grep processor /proc/cpuinfo | wc -l) otapackage 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+fi
 
 # clean out of previous zip
-ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+if [ "$BACON" = "true" ]; then
+    ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+else
+    ZIP=$(grep "Package OTA" "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f5 -d '/')
+fi
 OUTD=$(echo $(cd ../upload && pwd))
 rm $OUTD/$ZIP
 cp "$ANDROID_PRODUCT_OUT"/$ZIP $OUTD/$ZIP
