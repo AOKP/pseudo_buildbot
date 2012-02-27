18626cab0410f4edc30e1a04e95e5f6c08d931e4
Roman Birg <romanbirg@gmail.com>
2012-02-26 23:20:25 -0600

    merge from Khas

--- build_device.sh
+++ build_device.sh
@@ -9,25 +9,44 @@ cd $BUILD_ROOT
 lunch $1
 
 TARGET_VENDOR=$(echo $TARGET_PRODUCT | cut -f1 -d '_')
-CV=$(find vendor/$TARGET_VENDOR/ -name common_versions.mk)
-VER=$(cat /tank/roman/kang/vendor/aokp/configs/common_versions.mk | grep "TARGET_PRODUCT" | cut -f3 -d '_' | head -n 1)
+
+# bacon check
+if [ "$(grep -m 1 bacon build/envsetup.sh)" = "" ]; then
+    echo "Y U NO MAKE BACON?!"
+    BACON=false
+else
+    BACON=true
+fi
+
+# create log dir if not already present
+if test ! -d "$ANDROID_PRODUCT_OUT"
+    echo "$ANDROID_PRODUCT_OUT doesn't exist, creating now"
+    then mkdir -p "$ANDROID_PRODUCT_OUT"
+fi
 
 # build
-make -j$(grep processor /proc/cpuinfo | wc -l) bacon
+if [ "$BACON" = "true" ]; then
+    make -j$(($(grep processor /proc/cpuinfo | wc -l) * 2)) bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+else
+    make -j$(($(grep processor /proc/cpuinfo | wc -l) * 2)) otapackage 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+fi
 
 # clean out of previous zip
-ZIP=$(find $(echo $ANDROID_PRODUCT_OUT) -maxdepth 1 -name $(echo $TARGET_PRODUCT)_*-squished.zip)
+if [ "$BACON" = "true" ]; then
+    ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+else
+    ZIP=$(grep "Package OTA" "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f5 -d '/')
+fi
 OUTD=$(echo $(cd ../upload && pwd))
-OUTZ=$(echo $TARGET_PRODUCT)_$VER.zip
-rm -rf $OUTD/$OUTZ
-cp $ZIP $OUTD/$OUTZ
+rm $OUTD/$ZIP
+cp "$ANDROID_PRODUCT_OUT"/$ZIP $OUTD/$ZIP
 
 # finish
 echo "$2 build complete"
 
 # md5sum list
 cd $OUTD
-md5sum $OUTZ | cat >> md5sum
+md5sum $ZIP | cat >> md5sum
 
 # upload
 echo "checking on upload reference file"
@@ -35,12 +54,13 @@ echo "checking on upload reference file"
 BUILDBOT=$BUILD_ROOT/vendor/$TARGET_VENDOR/bot/
 cd $BUILDBOT
 if test -x upload ; then
-        echo "Upload file exists, executing now"
-        cp upload $OUTD
-        cd $OUTD
-        ./upload $2 && rm upload
+    echo "Upload file exists, executing now"
+    cp upload $OUTD
+    cd $OUTD
+    # device and zip names are passed on for upload
+    ./upload $2 $ZIP && rm upload
 else
-        echo "No upload file found (or set to +x), build complete."
+    echo "No upload file found (or set to +x), build complete."
 fi
 
 cd $BUILT_ROOT
