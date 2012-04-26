ab5e9cec932722c6af4d957e3be0022f563b4be4
KhasMek <Boushh@gmail.com>
2012-04-26 18:40:03 -0400

    Add clean and overlay options to Pseudo
      Allow Pseudo to rm -rf out/ before a build starts and add
      the -o option to call the vendorsetup.sh from a vendor other
      than aokp. This will enable themers that use source overlays
      to use Pseudo to build for all their supported devices using
      their overlay.
    
    Change-Id: I1ad81e3f139f1ccae23cd6b8c9e9f964f7d2575f
    Signed-off-by: KhasMek <Boushh@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,51 @@ cd $BUILD_ROOT
 repo sync
 . build/envsetup.sh
 
+# parse options
+while getopts ":c :o:" opt
+do
+    case "$opt" in
+        c) CLEAN=true;;
+        o)
+             THEME_VENDOR="$OPTARG"
+             echo "using $THEME_VENDOR vendorsetup.sh"
+        ;;
+        \?)
+             echo "invalid option: -$OPTARG"
+             echo "exiting..."
+             exit 1
+        ;;
+    esac
+done
+
 # check for clean
-if [ "$1" = "clean" ]; then
-	make clean
-	rm .bot_lunch
+if [ "$CLEAN" = "true" ]; then
+    echo "sanitizing build enviornment"
+    rm -rf out
+    rm .bot_lunch
 fi
 
 # find the ROM vendor from the manifest path for Pseudo
 ROM_VENDOR=$(grep pseudo_buildbot .repo/manifest.xml | cut -f4 -d ' ' | cut -f2 -d '/')
 
+# see if we are using a theme overlay or the ROM's vendorsetup
+if [ "$THEME_VENDOR" != "" ]; then
+    # using a theme overlay
+    VENDOR="$THEME_VENDOR"
+else
+    # find the ROM vendor from the manifest path for Pseudo
+    VENDOR="$ROM_VENDOR"
+fi
+
+# make sure file exists
+if [ ! -f vendor/$VENDOR/vendorsetup.shh ]; then
+    echo "vendorsetup.sh not found"
+    echo "exiting..."
+    exit 1
+fi
+
 # aokp_vzwtab-userdebug
-cat vendor/$ROM_VENDOR/vendorsetup.sh | cut -f2 -d ' ' > .bot_lunch
+cat vendor/$VENDOR/vendorsetup.sh | cut -f2 -d ' ' > .bot_lunch
 
 # build packages
 #
