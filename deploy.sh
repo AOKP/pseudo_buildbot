68e16bfac8ede7d34d7e36ef0fd2d6c9136ff42d
David Gasaway <ponsasinorem@gmail.com>
2012-09-15 17:18:23 -1000

    Don't overwrite the user's chosen DEVNAME
    Change-Id: I3164eec97a6cc3748a34b60395b10319827ea61d

--- deploy.sh
+++ deploy.sh
@@ -68,7 +68,7 @@ DEVNAME=$(echo $TARGET_PRODUCT-$TARGET_BUILD_VARIANT)
 # read the file and execute lunch
 while read line ;do
     # vzwtab
-    DEVNAME=$(echo $line | cut -f2 -d ' ' | cut -f2 -d '_' | cut -f1 -d '-')
+#    DEVNAME=$(echo $line | cut -f2 -d ' ' | cut -f2 -d '_' | cut -f1 -d '-')
     # build_device <lunch combo> <device name>
     ./vendor/$ROM_VENDOR/bot/build_device.sh $line $DEVNAME
 done < .bot_lunch
