bbf0a0a098051bcd4022b98efcef558b8bd2e927
Jason Stewart <sixstringsg@verizon.net>
2012-09-19 20:34:35 +0200

    pseudo - updates for more logging and less load
    This allows mka to be used when it is found (just like bacon)
    and also when mka isn't used, lower the load used on the build
    machine. Twice the number of build threads as cores causes
    really high I/O wait times, bogging down the box and slowing the
    build. With one extra jobs than threads, you have enough to saturate
    the processor without adding I/O wait time.
    
    Add more logging - add the log files to a directory where they will
    not be deleted by a clobber. This allows you to see what errors the
    build ran into after the fact, even if you clobber.
    
    Change-Id: Ib8ee575e9723de1485b2f60a81a09f4859846efc

--- build_device.sh
+++ build_device.sh
@@ -3,6 +3,7 @@
 # $1 should be lunch combo
 # $2 should be device name
 # select device and prepare varibles
+DATE=$(date +%h-%d-%y)
 BUILD_ROOT=`pwd`
 cd $BUILD_ROOT
 . build/envsetup.sh
@@ -18,25 +19,38 @@ else
     BACON=true
 fi
 
+# mka check
+if [ "$(grep -m 1 mka build/envsetup.sh)" = "" ]; then
+    echo "Y U WANT SLOW BUILD?!"
+    MKA=false
+else
+    MKA=true
+fi
+
 # create log dir if not already present
-if test ! -d "$ANDROID_PRODUCT_OUT"
-    echo "$ANDROID_PRODUCT_OUT doesn't exist, creating now"
-    then mkdir -p "$ANDROID_PRODUCT_OUT"
+if test ! -d logs
+    echo "log directory doesn't exist, creating now"
+    then mkdir -p logs
 fi
 
 # build
 if [ "$BACON" = "true" ]; then
-    make -j$(($(grep processor /proc/cpuinfo | wc -l) * 2)) bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+    if [ "$MKA" = "true" ];then
+        mka bacon 2>&1 | tee logs/"$TARGET_PRODUCT"_"$DATE"_bot.log
+    else
+        make $(($(grep processor /proc/cpuinfo | wc -l) + 1)) bacon 2>&1 | tee logs/"$TARGET_PRODUCT"_"$DATE"_bot.log
+    fi
 else
-    make -j$(($(grep processor /proc/cpuinfo | wc -l) * 2)) otapackage 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+    make $(($(grep processor /proc/cpuinfo | wc -l) + 1)) otapackage 2>&1 | tee logs/"$TARGET_PRODUCT"_"$DATE"_bot.log
 fi
 
 # clean out of previous zip
 if [ "$BACON" = "true" ]; then
-    ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+    ZIP=$(tail -2 logs/"$TARGET_PRODUCT"_"$DATE"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
 else
-    ZIP=$(grep "Package OTA" "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f5 -d '/')
+    ZIP=$(grep "Package OTA" logs/"$TARGET_PRODUCT"_"$DATE"_bot.log | cut -f5 -d '/')
 fi
+mkdir ../upload
 OUTD=$(echo $(cd ../upload && pwd))
 rm $OUTD/$ZIP
 cp "$ANDROID_PRODUCT_OUT"/$ZIP $OUTD/$ZIP
