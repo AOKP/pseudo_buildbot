449bad34063898e65dd86b27f0b4b6b1b467edbf
JBirdVegas <jbirdvegas@gmail.com>
2012-10-31 15:54:52 -0400

    DenseChangelog: JSON parser for repo
    deploy from repo root via
    denseChanglog.sh ~/path/to/output.json
    
    formulates a changelog based on git log --pretty output
    and parses into a JSON formated file.
    
    Patch Set 2: add terminal output about process
    Pathc Set 3: automate getting date of last update
                   parsed from goo.im lastest update to
                   'toro' branch
    
    Change-Id: I575d4babd655c9544a319e50b0c92793c7280ff4

---
+++ DenseChangelog/LastUpdateFinder.java
@@ -0,0 +1,35 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class LastUpdateFinder {
+    private static final File mLastUpdateFile = new File("last_update_time");
+
+    @SuppressWarnings("SimpleDateFormatWithoutLocale")
+    public static void main(String[] args) throws JSONException, IOException {
+        if (args[0] == null) {
+            return;
+        }
+
+        JSONArray input = new JSONObject(args[0]).getJSONArray("list");
+        long time = Long.parseLong((String) input.getJSONObject(0).get("modified")) * 1000;
+        DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(mLastUpdateFile);
+            fileWriter.append(df.format(time));
+        } finally {
+            fileWriter.close();
+        }
+        System.out.println("last update {" + df.format(time)
+                + "} has been written to file {" + mLastUpdateFile.getAbsolutePath()
+                + "}");
+    }
+}
+
