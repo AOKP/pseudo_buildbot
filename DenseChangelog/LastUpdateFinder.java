d8fbb3c636204b5b15946649696cc2f6a90de1ef
JBirdVegas <jbirdvegas@gmail.com>
2012-10-31 15:54:52 -0400

    DenseChangelog: JSON parser for repo
    deploy from repo root via
    denseChanglog.sh ~/path/to/output.json
    
    formulates a changelog based on git log --pretty output
    and parses into a JSON formated file.
    
    Patch Set 2: add terminal output about process
    Patch Set 3: automate getting date of last update
                   parsed from goo.im lastest update to
                   'toro' branch
    Patch Set 4: add a couple @SuppressWarnings
    Patch Set 5: better handling of script argment options
                   now using getopts
                 add -h flag for usage
                 renamed some vars to be more clear
    
    Change-Id: I575d4babd655c9544a319e50b0c92793c7280ff4

---
+++ DenseChangelog/LastUpdateFinder.java
@@ -0,0 +1,39 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+@SuppressWarnings("UtilityClass")
+public class LastUpdateFinder {
+    private static final File mLastUpdateFile = new File("last_update_time");
+
+    private LastUpdateFinder() {
+    }
+
+    @SuppressWarnings("SimpleDateFormatWithoutLocale")
+    public static void main(String... args) throws JSONException, IOException {
+        if (args[0] == null) {
+            return;
+        }
+
+        JSONArray input = new JSONObject(args[0]).getJSONArray("list");
+        long time = Long.parseLong((String) input.getJSONObject(0).get("modified")) * 1000;
+        DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(mLastUpdateFile);
+            fileWriter.append(df.format(time));
+        } finally {
+            if (fileWriter == null) {
+                throw new AssertionError();
+            }
+            fileWriter.close();
+        }
+        System.out.println("found date of last update: " + df.format(time));
+    }
+}
